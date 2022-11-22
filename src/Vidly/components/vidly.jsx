f62cc2efad84407355f3ddcb92326ad0a390e75b
Sincerely Brittany <sincerelybr@gmail.com>
2022-11-22 17:06:06 -0500

    adds table

--- src/Vidly/components/vidly.jsx
+++ src/Vidly/components/vidly.jsx
@@ -11,29 +11,40 @@ const Vidly = () => {
     };
     _getMovies();
   }, []);
+
+  const handleDeleteButton = (id) => {
+    console.log("here", id);
+  };
   return (
-    <>
-      <main className="container">
-        <table className="table table-light">
-          <thead>
-            <tr>
-              <th scope="col">Title</th>
-              <th scope="col">Genre</th>
-              <th scope="col">Stock</th>
-              <th scope="col">Rate</th>
-            </tr>
-          </thead>
-          <tbody>
-            {movies &&
-              movies.map((product) => (
-                <tr key={product._id}>
-                  <td>{product.title}</td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
-      </main>
-    </>
+    <main className="container">
+      <h1> Showing {movies && movies.length} movies in database.</h1>
+      <table className="table table-light">
+        <thead>
+          <tr>
+            <th scope="col">Title</th>
+            <th scope="col">Genre</th>
+            <th scope="col">Stock</th>
+            <th scope="col">Rate</th>
+          </tr>
+        </thead>
+        {movies &&
+          movies.map((movie) => (
+            <tbody>
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button onClick={() => handleDeleteButton(movie._id)}>
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          ))}
+      </table>
+    </main>
   );
 };
 
