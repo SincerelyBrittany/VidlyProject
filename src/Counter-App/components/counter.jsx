ce2e40b8ec77410a3d2acd0cdadb64e6555ce936
Sincerely Brittany <sincerelybr@gmail.com>
2022-11-23 00:09:36 -0500

    before removing state from counter component

--- src/Counter-App/components/counter.jsx
+++ src/Counter-App/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value.value,
+    value: this.props.counter.value,
     tags: [],
   };
 
@@ -39,7 +39,7 @@ class Counter extends Component {
           Increment
         </button>
         <button
-          onClick={() => this.props.onDelete(this.props.value.id)}
+          onClick={() => this.props.onDelete(this.props.counter.id)}
           className="btn btn-danger btn sm m-2"
         >
           {" "}
--- src/Counter-App/components/counters.jsx
+++ src/Counter-App/components/counters.jsx
@@ -33,14 +33,23 @@ class Counters extends Component {
     this.setState({ counters });
   };
 
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
   render() {
     return (
       <div>
         <h1 style={this.styles}> Counter Example </h1>
+        <button className="btn btn-primary btn-sm m-2">Reset </button>
         {this.state.counters.map((counter) => (
           <Counter
             key={counter.id}
-            value={counter}
+            counter={counter}
             onDelete={this.handleDelete}
           />
         ))}
