f9caa894e077f28b4b8a44920a4f6e012081b0f4
Sincerely Brittany <sincerelybr@gmail.com>
2022-12-02 23:06:03 -0500

    updates doSubmit/save movie to movie service

--- src/Vidly/services/movieService.js
+++ src/Vidly/services/movieService.js
@@ -10,7 +10,12 @@ export function getMovie(id) {
 }
 
 export function saveMovie(movie) {
-  console.log("in movie");
+  if (movie._id) {
+    const body = { ...movie };
+    delete body._id;
+    return http.put("http://localhost:3900/api/movies" + "/" + movie._id, body);
+  }
+  return http.post("http://localhost:3900/api/movies", movie);
 }
 
 export function deleteMovie(id) {
