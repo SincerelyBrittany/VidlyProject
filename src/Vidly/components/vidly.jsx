6e4a0e27900d156b66bdd43b9e9f8d3185a08e84
Sincerely Brittany <sincerelybr@gmail.com>
2022-12-02 22:48:44 -0500

    adds backend servies - movie and genre services' is not a valid branch name

--- src/Vidly/components/vidly.jsx
+++ src/Vidly/components/vidly.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import { getMovies } from "../services/fakeMovieService";
-import { getGenres } from "../services/fakeGenreService";
+import { getMovies } from "../services/movieService";
+import { getGenres } from "../services/genreService";
 import MovieTable from "./moviesTable";
 import Pagination from "../common/pagination";
 import ListGroup from "../common/listGroup";
 import { paginate } from "../../utils/paginate";
 import _ from "lodash";
+import { toast } from "react-toastify";
 import { Link } from "react-router-dom";
 import SearchBox from "./searchBox";
+import { deleteMovie } from "../services/movieService";
 
 const Vidly = () => {
   const [isDataLoading, setDataLoading] = React.useState(false);
@@ -23,8 +25,8 @@ const Vidly = () => {
   const [searchQuery, setSearchQuery] = React.useState("");
 
   const fetchMovies = async () => {
-    const response = await getMovies();
-    setMovies(response && response);
+    const { data: movies } = await getMovies();
+    setMovies(movies);
     setDataLoading(false);
   };
   React.useEffect(() => {
@@ -33,19 +35,33 @@ const Vidly = () => {
   }, []);
 
   const fetchGenres = async () => {
-    const response = await getGenres();
-    const genres = [{ _id: "", name: "All Genres" }, ...response];
+    const { data } = await getGenres();
+    console.log(data, "this is data");
+    const genres = [{ _id: "", name: "All Genres" }, ...data];
     setGenres(genres || []);
-    setDataLoading(false);
+    // const response = await getGenres();
+    // const genres = [{ _id: "", name: "All Genres" }, ...response];
+    // setGenres(genres || []);
+    // setDataLoading(false);
   };
+
   React.useEffect(() => {
     setDataLoading(true);
     fetchGenres();
   }, []);
 
-  const handleDeleteButton = (movie) => {
-    const moviesCopy = movies.filter((m) => m._id !== movie._id);
+  const handleDeleteButton = async (movie) => {
+    const originalMovies = movies;
+    const moviesCopy = originalMovies.filter((m) => m._id !== movie._id);
     setMovies(moviesCopy);
+    try {
+      await deleteMovie(movie._id);
+    } catch (ex) {
+      if (ex.response && ex.response.status === 404) {
+        toast.error("this movie has already been deleted");
+        setMovies(originalMovies);
+      }
+    }
   };
 
   const handleLikedButton = (movie) => {
